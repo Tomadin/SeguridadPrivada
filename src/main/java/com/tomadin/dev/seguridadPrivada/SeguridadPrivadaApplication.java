3e4624ab184fd15893edd36d8c57432b9a3ba201
Tomadin <tomadinbrian@gmail.com>
2025-11-08 00:02:22 -0300

    Configuración inicial

---
+++ src/main/java/com/tomadin/dev/seguridadPrivada/SeguridadPrivadaApplication.java
@@ -0,0 +1,13 @@
+package com.tomadin.dev.seguridadPrivada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeguridadPrivadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeguridadPrivadaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomadin/dev/seguridadPrivada/SeguridadPrivadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomadin.dev.seguridadPrivada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeguridadPrivadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
